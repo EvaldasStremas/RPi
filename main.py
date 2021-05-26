abe2eab73aca904a4bc8113412c80870ddb095d4
EvaldasStremas <evaldas.stremas123@gmail.com>
2021-05-26 17:24:40 +0300

    Telnet server chat; LED control;

--- main.py
+++ main.py
@@ -1,40 +1,117 @@
-"""
-https://pypi.org/project/telnetsrv/
-"""
+import logging
+from miniboa import TelnetServer
+import RPi.GPIO as GPIO
 
-import gevent, gevent.server
-from telnetsrv.green import TelnetHandler, command
-
-class MyTelnetHandler(TelnetHandler):
-    WELCOME = "Welcome to my server."
-
-    @command(['echo', 'copy', 'repeat'])
-    def command_echo(self, params):
-        '''<text to echo>
-        Echo text back to the console.
-
-        '''
-        self.writeresponse( ' '.join(params) )
-
-    @command('timer')
-    def command_timer(self, params):
-        '''<time> <message>
-        In <time> seconds, display <message>.
-        Send a message after a delay.
-        <time> is in seconds.
-        If <message> is more than one word, quotes are required.
-        example:
-        > TIMER 5 "hello world!"
-        '''
-        try:
-            timestr, message = params[:2]
-            time = int(timestr)
-        except ValueError:
-            self.writeerror( "Need both a time and a message" )
-            return
-        self.writeresponse("Waiting %d seconds...", time)
-        gevent.spawn_later(time, self.writemessage, message)
+IDLE_TIMEOUT = 300
+CLIENT_LIST = []
+SERVER_RUN = True
 
 
-server = gevent.server.StreamServer(("", 8023), MyTelnetHandler.streamserver_handle)
-server.serve_forever()
+def on_connect(client):
+    """
+    Sample on_connect function.
+    Handles new connections.
+    """
+    logging.info("Opened connection to {}".format(client.addrport()))
+    broadcast("{} joins the conversation.\n".format(client.addrport()))
+    CLIENT_LIST.append(client)
+    client.send("Welcome to the Chat Server, {}.\n".format(client.addrport()))
+
+
+def on_disconnect(client):
+    """
+    Sample on_disconnect function.
+    Handles lost connections.
+    """
+    logging.info("Lost connection to {}".format(client.addrport()))
+    CLIENT_LIST.remove(client)
+    broadcast("{} leaves the conversation.\n".format(client.addrport()))
+
+
+def kick_idle():
+    """
+    Looks for idle clients and disconnects them by setting active to False.
+    """
+    # Who hasn't been typing?
+    for client in CLIENT_LIST:
+        if client.idle() > IDLE_TIMEOUT:
+            logging.info("Kicking idle lobby client from {}".format(client.addrport()))
+            client.active = False
+
+
+def process_clients():
+    """
+    Check each client, if client.cmd_ready == True then there is a line of
+    input available via client.get_command().
+    """
+    for client in CLIENT_LIST:
+        if client.active and client.cmd_ready:
+            # If the client sends input echo it to the chat room
+            chat(client)
+
+
+def broadcast(msg):
+    """
+    Send msg to every client.
+    """
+    for client in CLIENT_LIST:
+        client.send(msg)
+
+
+def chat(client):
+    """
+    Echo whatever client types to everyone.
+    """
+    global SERVER_RUN
+    msg = client.get_command()
+    logging.info("{} says '{}'".format(client.addrport(), msg))
+
+    for guest in CLIENT_LIST:
+        if guest != client:
+            guest.send("{} says '{}'\n".format(client.addrport(), msg))
+        else:
+            guest.send("You say '{}'\n".format(msg))
+
+    cmd = msg.lower()
+    # bye = disconnect
+    if cmd == 'bye':
+        client.active = False
+    # shutdown == stop the server
+    elif cmd == 'shutdown':
+        SERVER_RUN = False
+    elif cmd == 'turn_led_on':
+        GPIO.output(13, GPIO.HIGH)
+    elif cmd == 'turn_led_off':
+        GPIO.output(13, GPIO.LOW)
+
+if __name__ == '__main__':
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(13, GPIO.OUT)
+
+    # Simple chat server to demonstrate connection handling via the
+    # async and telnet modules.
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    # Create a telnet server with a port, address,
+    # a function to call with new connections
+    # and one to call with lost connections.
+
+    telnet_server = TelnetServer(
+        port=7777,
+        address='',
+        on_connect=on_connect,
+        on_disconnect=on_disconnect,
+        timeout = .05
+        )
+
+    logging.info("Listening for connections on port {}. CTRL-C to break.".format(telnet_server.port))
+
+    # Server Loop
+    while SERVER_RUN:
+        telnet_server.poll()        # Send, Recv, and look for new connections
+        kick_idle()                 # Check for idle clients
+        process_clients()           # Check for client input
+
+    logging.info("Server shutdown.")
