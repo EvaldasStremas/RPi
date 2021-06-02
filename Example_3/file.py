3e7a5ef74770a49ff66840141a9ea54f67121661
EvaldasStremas <evaldas.stremas123@gmail.com>
2021-06-02 18:21:04 +0300

    Added pyserial; PWM objects;

--- Example_3/file.py
+++ Example_3/file.py
@@ -1,9 +1,29 @@
 import asyncio, telnetlib3
 import RPi.GPIO as GPIO
-from functions import Led
+from functions import Led, PwmLed
 
-led1 = Led('1', 0, '1', '0', 26)
-led2 = Led('1', 0, '2', '0', 13)
+#############################
+#1: led output status(0/100)
+#2: led press state (0/1)
+#3: keyboard expected input
+#4: keyboard pressed input
+#5: led pin
+#############################
+
+led1 = Led('0', 0, '1', '0', 26)
+led2 = Led('0', 0, '2', '0', 13)
+
+#############################
+#1: led output status(0/100)
+#3: keyboard pressed key
+#4: pwm up key
+#5  pwm down key
+#6: led pin
+#############################
+
+led3 = PwmLed(0, 19)
+
+# pwm_value = 0
 
 @asyncio.coroutine
 def shell(reader, writer):
@@ -16,13 +36,39 @@ def shell(reader, writer):
 
         led1.setNewKeyboardKey(keyboardInput)
         led1.simpleLedSwitch(led1.keyboard_input, led1.keyboard_pressed_key, led1.led_press_state)
-        led1.printAll(led1.led_light_state, led1.led_press_state, led1.keyboard_input, led1.keyboard_pressed_key, led1.led_pin)
+        # led1.printAll(led1.led_light_state, led1.led_press_state, led1.keyboard_input, led1.keyboard_pressed_key, led1.led_pin)
 
         led2.setNewKeyboardKey(keyboardInput)
         led2.simpleLedSwitch(led2.keyboard_input, led2.keyboard_pressed_key, led2.led_press_state)
-        led2.printAll(led2.led_light_state, led2.led_press_state, led2.keyboard_input, led2.keyboard_pressed_key, led2.led_pin)
+        # led2.printAll(led2.led_light_state, led2.led_press_state, led2.keyboard_input, led2.keyboard_pressed_key, led2.led_pin)
 
-        led1.getLedsStatus(led1.keyboard_pressed_key)
+        # led1.getLedsStatus(led1.keyboard_pressed_key)
+
+        #SIMPLE LED
+
+        if keyboardInput == 'y':
+            
+            print(led1.getLedOutputStatus())
+            print(led2.getLedOutputStatus())
+            print(led3.getPwmLedOutputStatus())
+            writer.write('\r\n' + str(led1.getLedOutputStatus()) + '\r')
+            writer.write('\r\n' + str(led2.getLedOutputStatus()) + '\r')
+            writer.write('\r\n' + str(led3.getPwmLedOutputStatus()) + '\r')
+
+        #PWM LED
+
+        if keyboardInput == '9':
+            pwmlvl = led3.setPwmRiseFive()
+            print(pwmlvl)
+
+        if keyboardInput == '6':
+            pwmlvl = led3.setPwmDropFive()
+            print(pwmlvl)
+
+        if keyboardInput == 'get_statistic':
+            led3.getPwmStatistic()
+
+        #EXIT
 
         if keyboardInput == '+':
             break
--- Example_3/functions.py
+++ Example_3/functions.py
@@ -2,6 +2,12 @@ import RPi.GPIO as GPIO
 
 
 class Led:
+    #1: led output status(0/100)
+    #2: led press state (0/1)
+    #3: keyboard expected input
+    #4: keyboard pressed input
+    #5: led pin
+
     def __init__(self, led_light_state, led_press_state, keyboard_expected_input, keyboard_pressed_key, led_pin):
         self.led_light_state = led_light_state
         self.led_press_state = led_press_state
@@ -12,6 +18,7 @@ class Led:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(led_pin, GPIO.OUT)
 
+
     def setLedOutputHigh(self, led_pin):
         GPIO.output(led_pin, GPIO.HIGH)
         self.led_light_state = 100
@@ -21,9 +28,16 @@ class Led:
         GPIO.output(led_pin, GPIO.LOW)
         self.led_light_state = 0
         self.led_press_state = 0
+    
+    def getLedOutputStatus(self):
+        return self.led_light_state
 
     def printAll(self, led_light_state, led_press_state, keyboard_expected_input, keyboard_pressed_key, led_pin):
-        print(led_light_state, led_press_state, keyboard_expected_input, keyboard_pressed_key, led_pin)
+        print(' LED status: ', str(led_light_state), '\n',
+        'LED press state: ', str(led_press_state), '\n',
+        'Keyboard expexted input: ' + str(keyboard_expected_input) + '\n',
+        'Keyboard pressed key: ' + str(keyboard_pressed_key) + '\n',
+        'LED pin: ' + str(led_pin) + '\n')
 
     def simpleLedSwitch(self, keyboard_input, keyboard_pressed_key, led_press_state):
         if keyboard_input == keyboard_pressed_key:
@@ -35,23 +49,47 @@ class Led:
     def setNewKeyboardKey(self, new_keyboard_key):
         self.keyboard_pressed_key = new_keyboard_key
 
-    def getLedsStatus(self, keyboard_pressed_key):
-        if keyboard_pressed_key == 's':
-            print("leds data")
+    # def getLedsStatus(self, keyboard_pressed_key):
+    #     if keyboard_pressed_key == 's':
+    #         print("leds data")
 
 class PwmLed:
-    def __init__(self, led_light_state, led_press_state, keyboard_pressed_key, pwm_up_key, pwm_down_key, led_pin):
-        self.led_light_state = led_light_state
-        self.led_press_state = led_press_state
-        self.keyboard_pressed_key = keyboard_pressed_key
-        self.pwm_up_key = pwm_up_key
-        self.pwm_down_key = pwm_down_key
-        self.led_pin = led_pin
+    #1: led output status(0/100)
+    #2: led press state (0/1)
+    #3: keyboard pressed key
+    #4: pwm up key
+    #5  pwm down key
+    #6: led pin
 
-    def risePwmLevel(self, pwm_up_key, keyboard_pressed_key):
-        if pwm_up_key == keyboard_pressed_key:
-            pass
+    def __init__(self, pwm_level, pwm_led_pin):
+        self.pwm_level = pwm_level
+        self.pwm_led_pin = pwm_led_pin
 
-    def downPwmLevel(self, pwm_down_key, keyboard_pressed_key):
-        if pwm_down_key == keyboard_pressed_key:
-            pass
+        global pwm
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(pwm_led_pin, GPIO.OUT)
+        pwm = GPIO.PWM(pwm_led_pin, 50)
+        pwm.start(0)
+
+
+    def setPwmRiseFive(self):
+        self.pwm_level += 5
+        pwm.ChangeDutyCycle(self.pwm_level)
+
+        return self.pwm_level
+
+
+    def setPwmDropFive(self):
+        self.pwm_level -= 5
+        pwm.ChangeDutyCycle(self.pwm_level)
+
+        return self.pwm_level
+
+
+    def getPwmLedOutputStatus(self):
+        return self.pwm_level
+    
+
+    def getPwmStatistic(self):
+        print('LED PWM duty cycle level: ', str(self.pwm_level), '\n',
+        'LED pin number: ', str(self.pwm_led_pin), '\n')
